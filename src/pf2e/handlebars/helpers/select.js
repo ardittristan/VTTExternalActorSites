2cda97ca48cc8ca269f462ec1ced1501ab0a38da
ardittristan <ardittristan@gmail.com>
2021-03-08 15:41:43 +0100

    remove duplicate handlebars dependency

--- src/pf2e/handlebars/helpers/select.js
+++ src/pf2e/handlebars/helpers/select.js
@@ -1,9 +1,11 @@
+const { escapeExpression } = require("handlebars");
+
 RegExp.escape = function (string) {
   return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 };
 
 module.exports = function (selected, options) {
-  const escapedValue = RegExp.escape(Handlebars.escapeExpression(selected));
+  const escapedValue = RegExp.escape(escapeExpression(selected));
   const rgx = new RegExp(' value="' + escapedValue + '"');
   const html = options.fn(this);
   return html.replace(rgx, "$& selected");
